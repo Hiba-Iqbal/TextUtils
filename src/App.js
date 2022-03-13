26b4732d1390759c224823af801b0d5ece5ca1c2
Your Name <hibaiqbal66@gmail.com>
2022-03-12 22:45:10 -0800

    NavBar Completed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import NavBar from "./components/NavBar";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <NavBar title={"TextUtils"} />
+    </div>
+  );
 }
 
 export default App;
